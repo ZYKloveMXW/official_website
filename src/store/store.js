0815fdf10ab3486ae08177d891085a15ec1d892b
ZYKloveMXW <508603401@qq.com>
2020-02-24 20:24:12 +0800

    搭建脚手架(路由,vuex已经搭好直接可以用

---
+++ src/store/store.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuex from 'Vuex'
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    // state == data
+    state: {
+    },
+    // getters 是对state中的数据进行处理
+    getters: {
+    },
+    // mutations 改变state里面的数据
+    mutations: {
+    }
+})
+
+export default store;
